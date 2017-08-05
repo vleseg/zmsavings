eb73e877ab7bd7db61c93f4f5ddbeb7a144a50ed
vleseg <vlesiil@yandex.ru>
2017-08-05 11:26:36 +0700

    Finish reimplementing utils module with unit tests

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,71 +1,162 @@
-import os
 # Third-party imports
 from mock import mock_open, patch
 import pytest
 # Project imports
-from zmsavings_new.utils import get_user_data_dir, path_from_appdata_or_input
+from zmsavings_new.utils import (
+    get_or_create_user_data_dir, read_path, read_yes_no,
+    path_from_appdata_or_input)
 
 
-@patch('zmsavings_new.utils.get_user_data_dir')
+class StopExecution(BaseException):
+    pass
+
+
 class TestPathFromAppdataOrInput(object):
     def setup(self):
         patcher = patch('zmsavings_new.utils.open', mock_open(), create=True)
         self._mock_open = patcher.start()
+        self._mock_open.return_value.read.return_value = 'path_from_ptr_file'
 
         # Other mocks
         self._mock_os = patch('zmsavings_new.utils.os', spec_set=True).start()
         self._mock_os.path.isfile.return_value = True
+        self._mock_read_yes_no = patch(
+            'zmsavings_new.utils.read_yes_no').start()
+        self._mock_get_or_create_udd = patch(
+            'zmsavings_new.utils.get_or_create_user_data_dir').start()
+        self._mock_get_or_create_udd.return_value = 'user_data_dir'
+        self._mock_read_path = patch('zmsavings_new.utils.read_path').start()
+        self._mock_read_path.return_value = 'new_path_to_csv'
 
-    def test_gets_path_to_ptr_file_by_name_and_opens_it(self, m_get_dir):
-        m_get_dir.return_value = 'bogus_parent'
-        path_from_appdata_or_input('bogus')
+    def test_gets_path_to_ptr_file_by_name_and_opens_it(self):
+        path_from_appdata_or_input('something')
 
-        m_get_dir.assert_called_once()
-        self._mock_os.path.join.assert_called_once_with('bogus_parent', 'bogus')
+        self._mock_get_or_create_udd.assert_called_once()
+        self._mock_os.path.join.assert_called_once_with(
+            'user_data_dir', 'something')
         self._mock_open.assert_called_once_with(
-            self._mock_os.path.join.return_value, 'rb')
+            self._mock_os.path.join.return_value, 'r')
 
-    def test_read_path_to_csv_file_from_ptr_file_and_return_it(self, _):
-        self._mock_open.return_value.read.return_value = 'path_to_csv_file'
+    def test_read_path_to_csv_file_from_ptr_file_and_return_it(self):
+        self._mock_read_yes_no.return_value = True
         result = path_from_appdata_or_input('something')
 
-        assert result == 'path_to_csv_file'
+        assert result == 'path_from_ptr_file'
 
-    def test_asks_if_you_want_to_use_the_read_path(self, _):
-        pass
+    def test_asks_if_you_want_to_use_path_from_ptr(self):
+        path_from_appdata_or_input('something')
 
-    @patch('zmsavings_new.utils.read_path')
+        self._mock_read_yes_no.assert_called_once_with(
+            "Do you want to use file 'path_from_ptr_file'?")
+
+    def test_if_user_doesnt_want_to_use_path_from_ptr_prompts_another(self):
+        self._mock_read_yes_no.return_value = False
+        path_from_appdata_or_input('something')
+
+        self._mock_read_path.assert_called_once_with(
+            "Enter valid path to 'something'")
+
     def test_prompts_path_to_csv_file_if_pointer_file_nonexistent_returns_it(
-            self, m_read_path, _):
+            self):
         self._mock_os.path.isfile.return_value = False
         result = path_from_appdata_or_input('something')
 
-        m_read_path.assert_called_once_with("Enter valid path to 'something'")
-        assert result == m_read_path.return_value
+        self._mock_read_path.assert_called_once_with(
+            "Enter valid path to 'something'")
+        assert result == self._mock_read_path.return_value
 
-    def test_prompts_path_to_csv_file_if_path_in_pointer_file_is_wrong(self, _):
-        pass
+    @patch('zmsavings_new.utils.print_function')
+    def test_prompts_path_to_csv_file_if_path_in_pointer_file_is_wrong(
+            self, mock_print):
+        # First call checks for ptr file, second call checks for csv file
+        self._mock_os.path.isfile.side_effect = [True, False]
+        path_from_appdata_or_input('something')
 
-    def test_creates_pointer_file_if_did_not_exist(self, _):
-        pass
+        mock_print.assert_called_once_with(
+            "Stored path 'path_from_ptr_file' does not exist")
+        self._mock_read_path.assert_called_with(
+            "Enter valid path to 'something'")
 
-    def test_saves_correct_path_to_pointer_file(self, _):
-        pass
+    def test_creates_pointer_file_if_did_not_exist(self):
+        path_to_ptr_file = self._mock_os.path.join.return_value
+        self._mock_os.path.isfile.return_value = False
+        path_from_appdata_or_input('something')
+
+        self._mock_open.assert_called_once_with(path_to_ptr_file, 'w')
+
+    def test_saves_correct_path_to_pointer_file(self):
+        self._mock_os.path.isfile.return_value = False
+        path_from_appdata_or_input('something')
+
+        self._mock_open.return_value.write.assert_called_once_with(
+            'new_path_to_csv')
 
 
 @patch('zmsavings_new.utils.os', spec_set=True)
 @patch('zmsavings_new.utils.appdirs', spec_set=True)
 class TestGetUserDataDir(object):
     def test_gets_user_data_dir_from_appdirs_with_app_name(self, m_appdirs, _):
-        result = get_user_data_dir()
+        result = get_or_create_user_data_dir()
         m_appdirs.user_data_dir.assert_called_once_with('ZmSavings')
 
         assert result == m_appdirs.user_data_dir.return_value
 
     def test_if_user_data_dir_does_not_exist_creates_it(self, m_appdirs, m_os):
         m_os.path.isdir.return_value = False
-        result = get_user_data_dir()
+        result = get_or_create_user_data_dir()
         m_appdirs_rv = m_appdirs.user_data_dir.return_value
 
         m_os.makedirs.assert_called_once_with(m_appdirs_rv)
         assert result == m_appdirs_rv
+
+
+@patch('zmsavings_new.utils.print_function')
+@patch('zmsavings_new.utils.os', spec_set=True)
+@patch('zmsavings_new.utils.raw_input')
+class TestReadPath(object):
+    def test_adds_exit_instruction_to_prompt_and_reads_path_from_user(
+            self, mock_raw_input, _, _2):
+        result = read_path('enter path')
+
+        mock_raw_input.assert_called_with('enter path (Ctrl+C to abort) ')
+        assert result == mock_raw_input.return_value
+
+    def test_if_entered_path_does_not_exist_prompts_again(
+            self, mock_raw_input, mock_os, mock_print):
+        mock_os.path.isfile.return_value = False
+        # Must raise, because the function keeps asking for correct input
+        # indefinitely
+        mock_raw_input.side_effect = [0, 1, StopExecution]
+        with pytest.raises(StopExecution):
+            read_path('enter path')
+
+        mock_print.assert_called_with('File does not exist')
+        mock_raw_input.assert_called_with('enter path (Ctrl+C to abort) ')
+
+
+@patch('zmsavings_new.utils.raw_input')
+class TestReadYesNo(object):
+    def test_adds_instruction_to_prompt_and_reads_from_user(self,
+                                                            mock_raw_input):
+        # Must raise, because the function keeps asking for correct input
+        # indefinitely
+        mock_raw_input.side_effect = StopExecution
+        with pytest.raises(StopExecution):
+            read_yes_no('confirm something')
+
+        mock_raw_input.assert_called_with(
+            'confirm something (y/n or Ctrl+C to abort) ')
+
+    def test_y_return_true_n_return_false(self, mock_raw_input):
+        mock_raw_input.side_effect = ['y', 'Y', 'n', 'N']
+
+        assert read_yes_no('answer with y') is True
+        assert read_yes_no('answer with Y') is True
+        assert read_yes_no('answer with n') is False
+        assert read_yes_no('answer with N') is False
+
+    def test_prompts_again_on_other_answers(self, mock_raw_input):
+        mock_raw_input.side_effect = [
+            'yes', '123', 'please stop', '...', StopExecution]
+        with pytest.raises(StopExecution):
+            read_yes_no('no correct input')
--- zmsavings_new/utils.py
+++ zmsavings_new/utils.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import os
 # Third-party imports
 import appdirs
@@ -5,7 +6,7 @@ import appdirs
 APP_NAME = 'ZmSavings'
 
 
-def get_user_data_dir():
+def get_or_create_user_data_dir():
     user_data_dir = appdirs.user_data_dir(APP_NAME)
     if not os.path.isdir(user_data_dir):
         os.makedirs(user_data_dir)
@@ -14,14 +15,37 @@ def get_user_data_dir():
 
 
 def read_path(prompt):
-    pass
+    while True:
+        path = raw_input(prompt + ' (Ctrl+C to abort) ')
+        if os.path.isfile(path):
+            return path
+        print_function('File does not exist')
+
+
+def read_yes_no(prompt):
+    while True:
+        user_input = raw_input(prompt + ' (y/n or Ctrl+C to abort) ')
+
+        if user_input.lower() == 'y':
+            return True
+        elif user_input.lower() == 'n':
+            return False
 
 
 def path_from_appdata_or_input(filename):
-    path_to_ptr_file = os.path.join(get_user_data_dir(), filename)
+    path_to_ptr_file = os.path.join(get_or_create_user_data_dir(), filename)
 
     if os.path.isfile(path_to_ptr_file):
-        with open(path_to_ptr_file, 'rb') as f:
-            return f.read()
-    else:
-        return read_path("Enter valid path to '{0}'".format(filename))
+        with open(path_to_ptr_file, 'r') as f:
+            path = f.read()
+            if os.path.isfile(path):
+                if read_yes_no("Do you want to use file '{0}'?".format(path)):
+                    return path
+            else:
+                print_function("Stored path '{0}' does not exist".format(path))
+
+    with open(path_to_ptr_file, 'w') as f:
+        path = read_path("Enter valid path to '{0}'".format(filename))
+        f.write(path)
+
+        return path
