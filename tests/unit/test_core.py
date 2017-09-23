229b71286e1863eecfe3e470bc93e664f7b94d60
vleseg <vlesiil@yandex.ru>
2017-09-23 13:09:24 +0700

    Mock naming fix

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -12,9 +12,9 @@ class TestMain(object):
         self._m_goal = self._goal_patcher.start()
         self._m_goal.all = Mock(return_value=['goal_1', 'goal_2', 'goal_3'])
 
-    def test_calls_select_transactions_for_each_goal(self, m_select, _):
+    def test_calls_select_transactions_for_each_goal(self, mock_select, _):
         main()
-        assert m_select.mock_calls == [
+        assert mock_select.mock_calls == [
             call('goal_1'), call('goal_2'), call('goal_3')]
 
     def test_gets_all_goals(self, _, _2):
@@ -22,31 +22,33 @@ class TestMain(object):
         assert self._m_goal.all.called
 
     def test_inits_progressive_totals_with_goals_and_transactions(
-            self, m_select, m_pt):
+            self, mock_select, mock_pt):
         self._m_goal.all = Mock(return_value=['goal_1', 'goal_2', 'goal_3'])
-        m_select.side_effect = [
+        mock_select.side_effect = [
             ['tr_1_1', 'tr_1_2', 'tr_1_3'],
             ['tr_2_1', 'tr_2_2', 'tr_2_3'],
             ['tr_3_1', 'tr_3_2', 'tr_3_3']
         ]
         main()
-        assert m_pt.call_args_list == [
+        assert mock_pt.call_args_list == [
             call('goal_1', ['tr_1_1', 'tr_1_2', 'tr_1_3']),
             call('goal_2', ['tr_2_1', 'tr_2_2', 'tr_2_3']),
             call('goal_3', ['tr_3_1', 'tr_3_2', 'tr_3_3'])
         ]
 
-    def test_calculate_called_for_each_progressive_total(self, _, m_pt):
-        m_pt_instances = [Mock()] * 3
-        m_pt.side_effect = m_pt_instances
+    def test_calculate_called_for_each_progressive_total(self, _, mock_pt):
+        mock_pt_objects = [Mock()] * 3
+        mock_pt.side_effect = mock_pt_objects
         main()
-        assert all(m_pt_inst.calculate.called for m_pt_inst in m_pt_instances)
+        assert all(
+            mock_pt_obj.calculate.called for mock_pt_obj in mock_pt_objects)
 
-    def test_visualize_called_for_each_progressive_total(self, _, m_pt):
-        m_pt_instances = [Mock()] * 3
-        m_pt.side_effect = m_pt_instances
+    def test_visualize_called_for_each_progressive_total(self, _, mock_pt):
+        mock_pt_instances = [Mock()] * 3
+        mock_pt.side_effect = mock_pt_instances
         main()
-        assert all(m_pt_inst.visualize.called for m_pt_inst in m_pt_instances)
+        assert all(
+            mock_pt_obj.visualize.called for mock_pt_obj in mock_pt_instances)
 
     def teardown_method(self):
         self._goal_patcher.stop()
@@ -56,13 +58,13 @@ class TestMain(object):
 @patch('zmsavings_new.core.Transaction', spec_set=True)
 class TestSelectTransactions(object):
     def test_selects_acc_and_then_its_transactions_by_cryptic_callables(
-            self, m_tr, m_account):
-        m_account.select = Mock(return_value=['account'])
-        m_tr.select = Mock(return_value=['tr_1', 'tr_2', 'tr_3'])
+            self, mock_transaction, mock_account):
+        mock_account.select = Mock(return_value=['account'])
+        mock_transaction.select = Mock(return_value=['tr_1', 'tr_2', 'tr_3'])
 
         result = _select_transactions_for_goal('goal')
-        assert m_account.select.called
-        assert callable(m_account.select.call_args[0][0])
-        assert m_tr.select.called
-        assert callable(m_tr.select.call_args[0][0])
+        assert mock_account.select.called
+        assert callable(mock_account.select.call_args[0][0])
+        assert mock_transaction.select.called
+        assert callable(mock_transaction.select.call_args[0][0])
         assert result == ['tr_1', 'tr_2', 'tr_3']
